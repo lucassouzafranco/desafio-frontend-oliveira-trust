d16ea9e4db8ca6ae8619ad23c9d9168733c1c48b
Lucas Souza <lucassouzafranco1@gmail.com>
2023-07-16 10:08:03 -0300

    ✨ feat(menu): profile and logout icons
    Basic menu structure

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Menu.css'
 import logo from '../../assets/oliveira_trust_logo.png';
 import { Link } from 'react-router-dom';
+import avatar from '../../assets/profile_icon.png';
+import exitIcon from '../../assets/exit_icon.png';
 
 function Menu() {
   return (
@@ -10,6 +12,11 @@ function Menu() {
         <Link to='/'>
           <img className='logo' src={logo} alt='Oliveira Trust Logo' />
         </Link>
+        <div className='profile'>
+          <img className='avatar' src={avatar} alt='Avatar icon' />
+          <p className='username'>Username</p>
+          <img className='exitIcon' src={exitIcon} alt='Exit' />
+        </div>
       </div>
     </div>
   )
